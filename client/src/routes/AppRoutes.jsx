58dcf20d0e9c831413edbc078ffa25e462d1b61a
sangambhandari200162-collab <sangambhandari200162@gmail.com>
2026-09-18 19:47:29 +0800

    feat: Login and signup page UI completed

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,20 +1,34 @@
+
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "../Layouts/Layout.jsx";
-import Home from "../pages/Home.jsx";
+import Layout from "../Layouts/Layout";
+
+import Home from "../pages/Home";
+import Login from "../pages/authentication/Login";
+import Register from "../pages/authentication/Register";
 
 function AppRoutes() {
   return (
     <Routes>
 
+      {/* Pages with Navbar + Footer */}
       <Route element={<Layout />}>
-
         <Route path="/" element={<Home />} />
 
+        <Route path="/departments" element={<div>Departments</div>} />
+        <Route path="/services" element={<div>Services</div>} />
+        <Route path="/about" element={<div>About Us</div>} />
       </Route>
+
+
+      {/* Authentication Pages - NO Navbar / Footer */}
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
 
     </Routes>
   );
 }
 
 export default AppRoutes;
+
