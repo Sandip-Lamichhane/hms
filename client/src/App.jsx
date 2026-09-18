cd2ae54e28ca92b1ef45fe365ed0c0e6019d67ae
Samir <samir@Samirs-MacBook-Pro.local>
2026-09-18 13:45:01 +0545

    feat: Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast"
+import AppRoutes from "./routes/AppRoutes"
+function App() {
+
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+
+    return(
+        <>
+        <h1>Welocme to home page</h1>
+        </>
+    );
+}
